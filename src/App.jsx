415e3a8f82c2a40d48a1d56bc86504f40ec4be54
cedricwietkin <wietkin.c@gmail.com>
2023-07-10 16:50:08 +0200

    chgt

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css'
+import './App.css';
 function ToDoList() {
   const [todos, setTodos] = useState(['Acheter du pain', 'Prendre rendez-vous']);
   const [newTodo, setNewTodo] = useState('');
