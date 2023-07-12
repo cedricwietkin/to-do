8edc409d8b1a7fd4c3f59e1912599269acd20d88
cedricwietkin <wietkin.c@gmail.com>
2023-07-12 16:03:33 +0200

    fonctionnel

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/App.css';
 
-
-
-
 function ToDoList() {
-  const [todos, setTodos] = useState([
-    { value: 'pain', date: '2023-07-10' },
-    { value: 'chocolat', date: '2023-07-09' },
-  ]);
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem('todos')) || []
+  );
   const [newTodo, setNewTodo] = useState('');
 
   const handleInputChange = (e) => {
@@ -37,27 +33,36 @@ function ToDoList() {
   };
 
   const handleDeleteTodo = (index) => {
-    // Handle deleting todo item
-    console.log('Delete todo:', index);
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
   };
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <>
-        <div className="todo">
-        
-          <div className="todo_first">
-            <input
-              className="todo_input"
-              type="text"
-              value={newTodo}
-              onChange={handleInputChange}
-              placeholder="Veuillez rentrer un nouvel élément"
-            />
-            <button className="todo_button" onClick={handleAddTodo}>
-              ajouter
-            </button>
+      <div className="todo">
+        <div className="todo_first">
+          <input
+            className="todo_input"
+            type="text"
+            value={newTodo}
+            onChange={handleInputChange}
+            placeholder="Veuillez rentrer un nouvel élément"
+          />
+          <button className="todo_button" onClick={handleAddTodo}>
+            ajouter
+          </button>
+        </div>
+        <form className="table">
+          <div className="category">
+            <p></p>
+            <p>To-do</p>
+            <p>Date</p>
+            <p>Edit/Delete</p>
           </div>
-          <form>
           <ul className="list">
             {todos.map((todo, index) => (
               <li key={index} className="todo_item">
@@ -68,26 +73,25 @@ function ToDoList() {
                 />
                 <span className="todo_value">{todo.value}</span>
                 <span className="todo_date">{todo.date}</span>
-                <div className='todo_iconB'>
-                <button
-                  className="todo_iconButton"
-                  onClick={() => handleEditTodo(index)}
-                >
-                  <i className="fas fa-edit"></i>
-                </button>
-                <button
-                  className="todo_iconButton"
-                  onClick={() => handleDeleteTodo(index)}
-                >
-                  <i className="fas fa-trash"></i>
-                </button>
+                <div className="todo_iconB">
+                  <button
+                    className="todo_iconButton"
+                    onClick={() => handleEditTodo(index)}
+                  >
+                    <i className="fas fa-edit"></i>
+                  </button>
+                  <button
+                    className="todo_iconButton"
+                    onClick={() => handleDeleteTodo(index)}
+                  >
+                    <i className="fas fa-trash"></i>
+                  </button>
                 </div>
               </li>
             ))}
           </ul>
-          </form>
-        </div>
-     
+        </form>
+      </div>
     </>
   );
 }
---
+++ src/jsx/calendar.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+export default function calendar(){
+    return(
+        <>
+          <section className='whiteBox'>
+            <div>
+                <i></i>
+                <div>
+                    <p>date</p>
+                    <p>année</p>
+                </div>
+                <i></i>
+            </div>
+            <form>
+                
+            </form>
+          </section>  
+        </>
+
+    );
+}
